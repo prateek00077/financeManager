2d1fdf76d5ab8ad658f34d83f59613878d956709
prateek00077 <pandeyprateek560@gmail.com>
2025-09-11 00:54:44 +0530

    added the basic files and setup tailwindcss

---
+++ frontend/src/Form.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <form action="">
+        <div>
+            <label htmlFor="">title</label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor="">amount</label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor="">date</label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor="">category</label>
+            <input type="text" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Form
